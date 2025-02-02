1d62e42f26b5ac0564a408ae910fc749c1d816eb
SathyaMol93 <masg.molagoda@gmail.com>
2025-02-02 14:15:11 +0400

    Theming

--- docs/components/Logo.vue
+++ docs/components/Logo.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="flex">
-    <img width="120" src="/img/logo.svg" />
-    <p>ZyphyrJs</p>
+  <div class="flex space-x-1">
+    <p class="font-bold text-xl"><span class="text-primary">Zyphyr</span><span class="text-secondary">Js</span></p>
   </div>
 </template>
